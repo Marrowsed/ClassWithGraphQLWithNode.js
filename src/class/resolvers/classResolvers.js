ac10e162d57c3be2abd9be8138d0dc952eb8d05f
Marrowsed <marcus.ah@gmail.com>
2022-11-06 09:15:56 -0300

    Class Schema, Resolver and DataSource

---
+++ src/class/resolvers/classResolvers.js
@@ -0,0 +1,29 @@
+const {GraphQLScalarType } = require('graphql')
+
+const userResolver = {
+    ClassTime: {
+        MORNING: "MORNING",
+        EVENING: "EVENING",
+        NIGHT: "NIGHT"
+    },
+    DateTime: new GraphQLScalarType({
+        name: 'DateTime',
+        description: 'Date and Time String',
+        serialize: (value) => value.toISOString(),
+        parseValue: (value) => new Date(value),
+        parseLiteral: (ast) => new Date(ast.value)
+    }),
+    Query: {
+        classes: (_, __, {dataSources }) => dataSources.classesAPI.getClasses(),
+        classesId: (_, {id}, {dataSources }) => dataSources.classesAPI.getClassesId(id),
+    },
+    Mutation: {
+        addClass: (_, {classes}, {dataSources }) => dataSources.classesAPI.postClasses(classes),
+        updateClass: (_, newData, {dataSources }) => dataSources.classesAPI.updateClasses(newData),
+        deleteClass:(_, {id}, {dataSources }) => dataSources.classesAPI.deleteClasses(id),
+        
+    }
+
+}
+
+module.exports = userResolver
--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
 const { ApolloServer } = require('apollo-server')
+const { mergeTypeDefs } = require('@graphql-tools/merge')
 const userSchema = require('./user/schema/user.graphql')
 const userResolver = require('./user/resolvers/userResolvers')
 const UsersAPI = require('./user/datasource/user.js')
 
-const typeDefs = [userSchema]
-const resolvers = [userResolver]
+const classSchema = require('./class/schema/class.graphql')
+const classResolver = require('./class/resolvers/classResolvers')
+const ClassesAPI = require('./class/datasource/classes.js')
+
+
+
+const typeDefs = mergeTypeDefs([userSchema, classSchema])
+const resolvers = [userResolver, classResolver]
 
 const server = new ApolloServer(
     {typeDefs,
     resolvers, 
     dataSources: () => {
         return  {
-            usersAPI: new UsersAPI()
+            usersAPI: new UsersAPI(),
+            classesAPI: new ClassesAPI()
         }
     }});
 
